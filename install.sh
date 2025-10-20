d3bed0e9e612dacb98a46fbafc4b58742efce8ba
ffaba <ffabama@gmail.com>
2025-10-20 17:51:40 -0300

    -

--- install.sh
+++ install.sh
@@ -32,19 +32,19 @@ PAQUETES_AUR=(
 echo "-- Iniciando la instalación de los paquetes .."
 sudo pacman -Syu --needed --noconfirm "${PAQUETES[@]}"
 
+# --- INSTALACION Dotfiles ---
+echo "-- Iniciando la instalación de los Dotfiles .."
+
+rm -r ../.config/hypr
+cp -r wallpaper/ ../.config/
+cp -r hypr/ ../.config/
+cp -r kitty/ ../.config/
+cp -r waybar/ ../.config/
+
+
 # --- INSTALACIÓN de YAY y PAQUETES_AUR ---
 
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Syu --needed --noconfirm "${PAQUETES_AUR[@]}"
 
-# --- INSTALACION Dotfiles ---
-echo "-- Iniciando la instalación de los Dotfiles .."
-
-rm -r ../.config/hypr
-cp -r hypr/ ../.config/
-cp -r kitty/ ../.config/
-cp -r waybar/ ../.config/
-
-echo "-- Instalacion 100%"
-
-
+reboot
