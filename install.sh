17d747ebf20bff6209c3e16b19b345e99b85d5aa
ffaba <ffabama@gmail.com>
2025-10-20 13:54:49 -0300

    -

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Este script detendrá la ejecución si algún comando falla.
+set -e
+
+# --- LISTA DE PAQUETES A INSTALAR ---
+PAQUETES=(
+    alsa-utils
+    bluez
+    bluez-tools
+    bluez-utils
+    code
+    pavucontrol
+    pipewire
+    pipewire-alsa
+    pipewire-jack
+    pipewire-pulse
+    rofi
+    unzip
+    waybar
+    wget
+    wireplumber
+)
+PAQUETES_AUR=(
+    hyprpaper
+    sublime-text-4
+    getnf
+)
+
+# --- INSTALACIÓN de PAQUETES ---
+
+echo "-- Iniciando la instalación de los paquetes .."
+sudo pacman -Syu --needed --noconfirm "${PAQUETES[@]}"
+
+# --- INSTALACIÓN de YAY y PAQUETES_AUR ---
+
+sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -Syu --needed --noconfirm "${PAQUETES_AUR[@]}"
+
+# --- INSTALACION Dotfiles ---
+echo "-- Iniciando la instalación de los Dotfiles .."
+
+rm -r ../.config/hypr/
+cp -r hypr/ ../.config/
+cp -r kitty ../.config/
+cp -r waybar ../.config/
+
+
+
